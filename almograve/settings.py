3bd3260fe1433416664b10af32f236e53bb000fc
Anže Pečar <anze@zenput.com>
2021-08-30 02:28:05 +0100

    Add pylint and isort

--- almograve/settings.py
+++ almograve/settings.py
@@ -9,14 +9,16 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+
+
+from pathlib import Path
+
 import environ
 
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
-
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
