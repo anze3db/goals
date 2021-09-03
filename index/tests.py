1ad1af20c2327b67f891821e00b49816c2a92ce5
Anže Pečar <anze@zenput.com>
2021-09-03 01:57:36 +0100

    Fix pylint issues

--- index/tests.py
+++ index/tests.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 
-from goals.factories import BoardFactory
 from users.factories import UserFactory
 
 
--- index/views.py
+++ index/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import redirect, render
 
-from users.models import User
-
 
 def index_view(request):
     if request.user.is_anonymous:
-        return render(request, "index.html", dict())
+        return render(request, "index.html", {})
 
     return redirect("/boards")
