26e73b1bc52de7433d03b73ca39d42054d1e2998
Anže Pečar <anze@zenput.com>
2021-09-08 01:56:04 +0100

    Fix statc file generation on heroku

--- almograve/settings.py
+++ almograve/settings.py
@@ -24,6 +24,8 @@ env = environ.Env(
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
