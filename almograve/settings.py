7f122291ce654d3826b1e0fd4df044ac1c25a2e4
Anže Pečar <anze@zenput.com>
2021-09-14 01:28:17 +0100

    Add sentry

--- almograve/settings.py
+++ almograve/settings.py
@@ -19,6 +19,7 @@ env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False),
     ALLOWED_HOSTS=(str, ""),
+    SENTRY_DNS=(str, ""),
 )
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -143,3 +144,19 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+if SENTRY_DNS := env("SENTRY_DNS"):
+    sentry_sdk.init(
+        dsn=SENTRY_DNS,
+        integrations=[DjangoIntegration()],
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True,
+    )
