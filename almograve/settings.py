566b8bbf6fe0e78fba6e3d73781628a795522c9a
Anže Pečar <anze@zenput.com>
2021-09-14 01:17:27 +0100

    Add tailwind

--- almograve/settings.py
+++ almograve/settings.py
@@ -51,9 +51,15 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "tailwind",
+    "theme",
     "users",
     "index",
     "goals",
+]
+TAILWIND_APP_NAME = "theme"
+INTERNAL_IPS = [
+    "127.0.0.1",
 ]
 
 MIDDLEWARE = [
