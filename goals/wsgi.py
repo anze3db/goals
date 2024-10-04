c88a1e8d38fea7307539ed63ad99f91ad55aa8dc
Anže Pečar <anze@pecar.me>
2024-10-04 22:09:55 +0100

    Move almograve -> goals

--- goals/wsgi.py
+++ goals/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for almograve project.
+WSGI config for goals project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "almograve.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "goals.settings")
 
 application = get_wsgi_application()
