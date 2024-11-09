770dcce18b53199ac0aa693d6921ec221ea17d52
Anže Pečar <anze@pecar.me>
2024-11-09 00:35:58 +0000

    Stop using pid files

--- .deploy/update.sh
+++ .deploy/update.sh
@@ -5,6 +5,6 @@ git pull
 uv sync --locked
 uv run python manage.py collectstatic --noinput
 uv run python manage.py migrate
-sudo kill -hup `cat /var/run/gunicorn-goals.pid`
+ps axf | grep 'gunicorn: master \[goals\]' | awk '{print "sudo kill -hup " $1}' | sh
 echo `date "+%Y-%m-%d %H:%M:%S.%3N"` ' Updated' >> update.log
 popd
--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,4 @@
+proc_name = "goals"
 bind = "unix:goals.sock"
 workers = 1
 threads = 4
-proc_name = "goals"
-pidfile = "/var/run/gunicorn-goals.pid"
