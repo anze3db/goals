429de940c473cfd502c9ecb32d7081462e0ba60e
Anže Pečar <anze@pecar.me>
2024-11-10 01:20:44 +0000

    Improve goals service

--- .deploy/update.sh
+++ .deploy/update.sh
@@ -5,6 +5,6 @@ git pull
 uv sync --locked
 uv run python manage.py collectstatic --noinput
 uv run python manage.py migrate
-ps axf | grep 'gunicorn: master \[goals\]' | awk '{print "sudo kill -hup " $1}' | sh
+sudo systemctl reload goals
 echo `date "+%Y-%m-%d %H:%M:%S.%3N"` ' Updated' >> update.log
 popd
